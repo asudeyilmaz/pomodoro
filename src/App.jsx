b0db1730e958b7cedcd58bdc2bed4c43fc208408
Asude <asudeyilmaz76@gmail.com>
2025-11-05 18:54:05 +0300

    style: CountDown.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import {useEffect, useState} from "react";
 import "./App.css";
-import {useDispatch, useSelector} from "react-redux";
-import {pomodoroStart, tick} from "./redux/pomodoroSlice";
+import CountDown from "./components/CountDown";
 
 function App() {
-  const {run, timer} = useSelector((state) => state.pomodoro);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    let interval;
-    if (run) {
-      interval = setInterval(() => {
-        dispatch(tick());
-      }, 1000);
-    }
-    return () => clearInterval(interval);
-  }, [run, dispatch]);
-
-  const minutes = Math.floor(timer / 60);
-  const seconds = timer % 60;
   return (
     <div>
-      <h1>{`${minutes}dk ${seconds}sn`} </h1>
-      <button onClick={() => dispatch(pomodoroStart())}>başlat</button>
+      <CountDown />
     </div>
   );
 }
---
+++ src/components/CountDown.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {pomodoroStart, tick} from "../redux/pomodoroSlice";
+function CountDown() {
+  const {run, timer} = useSelector((state) => state.pomodoro);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let interval;
+    if (run) {
+      interval = setInterval(() => {
+        dispatch(tick());
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [run, dispatch]);
+
+  const minutes = Math.floor(timer / 60);
+  const seconds = timer % 60;
+  return (
+    <div>
+      <h1>{`${minutes}:${seconds}`} </h1>
+      <button onClick={() => dispatch(pomodoroStart())}>başlat</button>
+    </div>
+  );
+}
+
+export default CountDown;
